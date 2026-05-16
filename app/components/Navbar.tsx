36b5c5cd664c204f474f822ddaa3114c9598dc30
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-05-16 04:03:38 -0700

    yangilandi

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,25 +3,25 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div className="h-12 bg-white shadow-md">
-           <div className="flex justify-around">
-             <div className="items-center p-2 pl-10">
-               <h1 className="text-black font-bold font-serif text-2xl ">T E S L A</h1>
+            <div className="flex justify-around">
+                <div className="items-center p-2 pl-10">
+                    <h1 className="text-black font-bold font-serif text-2xl ">T E S L A</h1>
+                </div>
+                <div className="flex bg-white gap-10 text-black font-bold font-mono items-center ">
+                    <Link className="bg-white hover:bg-gray-200 w-25 p-1 text-center rounded-lg" href={'/'}>Vehicles</Link>
+                    <Link className="bg-white hover:bg-gray-200 w-20 p-1 text-center rounded-lg" href={'/'}>Energy</Link>
+                    <Link className="bg-white hover:bg-gray-200 w-25 p-1 text-center rounded-lg" href={'/'}>Charging</Link>
+                    <Link className="bg-white hover:bg-gray-200 w-25 p-1 text-center rounded-lg" href={'/'}>Discover</Link>
+                    <Link className="bg-white hover:bg-gray-200 w-18 p-1 text-center rounded-lg" href={'/'}>Shop</Link>
+                </div>
+                <div className="flex gap-5 items-center">
+                    <Link href={'/'}>
+                        <img className=" w-6 p-1 border-black border-1 rounded-4xl" src="https://cdn-icons-png.flaticon.com/512/348/348488.png" alt="" />
+                    </Link>
+                    <Link href={'/'}> <img className="w-8 pl-2" src="https://logodix.com/logo/862817.png" alt="" /></Link>
+                    <Link href={'/'}><img className=" w-10 " src="https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small_2x/user-icon-on-transparent-background-free-png.png" alt="" /></Link>
+                </div>
             </div>
-            <div className="flex gap-10 text-black font-bold font-mono items-center ">
-                <Link href={'/'}>Vehicles</Link>
-                <Link href={'/'}>Energy</Link>
-                <Link href={'/'}>Charging</Link>
-                <Link href={'/'}>Discover</Link>
-                <Link href={'/'}>Shop</Link>
-            </div>
-            <div className="flex gap-5 items-center">
-                <Link href={'/'}>
-                <img className="w-6 p-1 border-black border-1 rounded-4xl" src="https://cdn-icons-png.flaticon.com/512/348/348488.png" alt="" />
-                </Link>
-                <Link href={'/'}> <img className="w-8 pl-2" src="https://logodix.com/logo/862817.png" alt="" /></Link>
-                <Link href={'/'}><img className="w-10 " src="https://static.vecteezy.com/system/resources/thumbnails/019/879/186/small_2x/user-icon-on-transparent-background-free-png.png" alt="" /></Link>
-            </div>
-           </div>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
                     <img className="absolute object-cover" src="https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Promo-Model-3-Desktop-US-CA-MX.jpg" alt="" />
                     <div className="relative z-10 pt-15">
                         <h1 className="text-5xl font-bold text-white text-center">Model 3</h1>
-                        <h1 className="text-xl font-semibold text-white text-center">One year of the Supercharging included¹</h1>
+                        <h1 className="text-xl font-semibold text-white text-center pt-2">One year of the Supercharging included¹</h1>
                     </div>
-                    <div className="relative z-10 flex gap-2 justify-center pt-5">
-                        <Link href={'/'}><h1 className="bg-blue-500 hover:bg-blue-600 w-50 text-lg font-bold font-mono text-center text-white rounded-lg p-1">Order Now</h1></Link>
-                        <Link href={'/'}><h1 className="bg-gray-100 hover:bg-gray-200 w-50 text-lg font-bold font-mono text-center text-gray-800 rounded-lg p-1">Learn More</h1></Link>
+                    <div className="relative z-10 flex gap-2 justify-center pt-8">
+                        <Link href={'/'}><h1 className="bg-blue-500 hover:bg-blue-600 w-50 font-bold text-center text-gray-100 rounded-lg p-2">Order Now</h1></Link>
+                        <Link href={'/'}><h1 className="bg-gray-100 hover:bg-gray-300 w-50 font-bold text-center text-gray-800 rounded-lg p-2">Learn More</h1></Link>
                     </div>
                 </div>
 
